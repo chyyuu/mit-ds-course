585697f9d29c878c8620ced50a59af93a091130f
Robert Morris <rtm@amsterdam.lcs.mit.edu>
2013-01-09 17:18:12 -0500

    update

--- src/paxos/paxos.go
+++ src/paxos/paxos.go
@@ -1,12 +1,16 @@
 package paxos
 
 //
-// Paxos library
+// Paxos library, to be included in an application.
+// Multiple applications will run, each including
+// a Paxos peer.
 //
 // Manages a sequence of agreed-on values.
 // The set of peers is fixed.
 // Copes with network failures (partition, msg loss, &c).
-// Does not store anything persistently, so cannot really handle crashes.
+// Does not store anything persistently, so cannot handle crash+restart.
+//
+// The application interface:
 //
 // px = paxos.Make(peers []string, me string)
 // px.Max() int -- highest instance seq known, or -1
@@ -16,3 +20,153 @@ package paxos
 // px.Done(seq int) -- ok to GC all instances <= seq
 //
 
+import "net"
+import "net/rpc"
+import "log"
+import "os"
+import "syscall"
+import "sync"
+import "fmt"
+import "math/rand"
+
+
+type Paxos struct {
+  mu sync.Mutex
+  l net.Listener
+  dead bool
+  unreliable bool
+  peers []string
+  me int // index into peers[]
+
+
+  // Your data here.
+}
+
+
+//
+// the application wants paxos to start agreement on
+// instance seq, with proposed value v.
+// Start() returns right away; the application will
+// call Get() to find out if/when agreement
+// is reached.
+//
+func (px *Paxos) Start(seq int, v interface{}) {
+  // Your code here.
+}
+
+//
+// the application on this machine is done with
+// all instances <= seq.
+//
+func (px *Paxos) Done(seq int) {
+  // Your code here.
+}
+
+//
+// the application wants to know the
+// highest instance sequence known to
+// this peer.
+//
+func (px *Paxos) Max() int {
+  // Your code here.
+  return 0
+}
+
+//
+// the application wants to know how far
+// garbage collection has progressed on this peer;
+// paxos has discarded all instances < Min().
+//
+func (px *Paxos) Min() int {
+  // You code here.
+  return 0
+}
+
+//
+// the application wants to know whether this
+// peer thinks an instance has been decided,
+// and if so what the agreed value is.
+//
+func (px *Paxos) Get(seq int) (bool, interface{}) {
+  // Your code here.
+  return false, nil
+}
+
+
+//
+// tell the peer to shut itself down.
+// for testing.
+// please do not change this function.
+//
+func (px *Paxos) Kill() {
+  px.dead = true
+  if px.l != nil {
+    px.l.Close()
+  }
+}
+
+//
+// the application wants to create a paxos peer.
+// the ports of all the paxos peers (including this one)
+// are in peers[]. this server's port is peers[me].
+//
+func Make(peers []string, me int, rpcs *rpc.Server) *Paxos {
+  px := &Paxos{}
+  px.peers = peers
+  px.me = me
+
+
+  // Your initialization code here.
+
+  if rpcs != nil {
+    // caller will create socket &c
+    rpcs.Register(px)
+  } else {
+    rpcs = rpc.NewServer()
+    rpcs.Register(px)
+
+    // prepare to receive connections from clients.
+    // change "unix" to "tcp" to use over a network.
+    os.Remove(peers[me]) // only needed for "unix"
+    l, e := net.Listen("unix", peers[me]);
+    if e != nil {
+      log.Fatal("listen error: ", e);
+    }
+    px.l = l
+    
+    // please do not change any of the following code.
+    
+    // create a thread to accept RPC connections
+    go func() {
+      for px.dead == false {
+        conn, err := px.l.Accept()
+        if err == nil && px.dead == false {
+          if px.unreliable && (rand.Int63() % 1000) < 100 {
+            // discard the request.
+            conn.Close()
+          } else if px.unreliable && (rand.Int63() % 1000) < 200 {
+            // process the request but force discard of reply.
+            c1 := conn.(*net.UnixConn)
+            f, _ := c1.File()
+            err := syscall.Shutdown(int(f.Fd()), syscall.SHUT_WR)
+            if err != nil {
+              fmt.Printf("shutdown: %v\n", err)
+            }
+            go rpcs.ServeConn(conn)
+          } else {
+            go rpcs.ServeConn(conn)
+          }
+        } else if err == nil {
+          conn.Close()
+        }
+        if err != nil && px.dead == false {
+          fmt.Printf("Paxos(%v) accept: %v\n", me, err.Error())
+          px.Kill()
+        }
+      }
+    }()
+  }
+
+
+  return px
+}
